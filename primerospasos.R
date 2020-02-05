f3d200bb60e479ae53c5b487a69d18a2f7bfe7e9
Mario A. Garcia-Meza <marius.agm@gmail.com>
2020-02-04 21:58:56 -0600

    Primeros pasos de un economista

---
+++ primerospasos.R
@@ -0,0 +1,17 @@
+# El primer programa de todo programador en un lenguaje de programacion nuevo
+print("Hola mundo") # Usa ctrl + Enter para ejecutar este código.
+
+2+2 # R puede servir como una computadora super increible
+
+x <- 2 # Puedes guardar valores en letras.
+# normalmente les llamamos variables
+
+x + 2 # Dale ctrl + enter!
+
+x <- c(1,2,3) # las listas les llamamos vectores
+
+x^2 # puedes hacer operaciones con vectores
+
+x + 1:3 # Puedes explicar que pasó aquí?
+
+
